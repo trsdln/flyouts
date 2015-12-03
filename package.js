fad2cf9441d0e56bb3033a35e40ad3662a060c05
Taras Dolynyuk <trsdln@gmail.com>
2015-12-03 13:26:39 +0200

    Add meteor@1.2 support

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'trsdln:flyouts',
-  version: '0.2.1',
+  version: '1.0.0',
   // Brief, one-line summary of the package.
   summary: 'Easy customizable and reactive sidebar flyouts',
   // URL to the Git repository containing the source code for this package.
@@ -11,7 +11,7 @@ Package.describe({
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom('1.0');
+  api.versionsFrom('1.2');
   api.use([
     'mongo',
     'templating',
