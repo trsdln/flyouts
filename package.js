b917d9fdc79300c9d1b890ac3f424736d42b7c84
Taras Dolynyuk <trsdln@gmail.com>
2016-04-15 15:24:23 +0300

    Increase version before publish

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'trsdln:flyouts',
-  version: '1.1.1',
+  version: '1.1.2',
   // Brief, one-line summary of the package.
   summary: 'Easy customizable and reactive sidebar flyouts',
   // URL to the Git repository containing the source code for this package.
